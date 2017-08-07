9291909b006d1e9e6433d21731f53f0877a56e2b
Jia Shi <fdushijia@163.com>
2017-08-07 16:43:59 +0800

    code format

--- background.js
+++ background.js
@@ -11,29 +11,27 @@ window.onload = function() {
 	} else {
 		console.log("Creating table if not exists");
 		db.transaction(function (context) {
-           context.executeSql('CREATE TABLE IF NOT EXISTS TagHistory (url text, tags text, timestamp text)');
-        });
+			context.executeSql('CREATE TABLE IF NOT EXISTS TagHistory (url text, tags text, timestamp text)');
+		});
 
 	}
 
 }
 chrome.webNavigation.onCompleted.addListener(function(tab){
 	// TODO： remove duplicate urls in one process
-    if (tab.url.search("baidu") == -1 && tab.url.search("google") == -1 && tab.url.search("about:blank") == -1) {
-    	var tags = "test";
-    	/*var db = new SQL.Database();*/
-    	console.log("insert for" + tab.url);
+	if (tab.url.search("baidu") == -1 && tab.url.search("google") == -1 && tab.url.search("about:blank") == -1) {
+		var tags = "test";
+		console.log("insert for" + tab.url);
 		db.transaction(function (context) {
-           context.executeSql("INSERT INTO TagHistory (url, tags, timestamp) VALUES ('" + tab.url + "','" + tags + "','" + tab.timeStamp + "');");
-        });
+			context.executeSql("INSERT INTO TagHistory (url, tags, timestamp) VALUES ('" + tab.url + "','" + tags + "','" + tab.timeStamp + "');");
+		});
 
-        /*db.readTransaction(function(t){
-   			t.executeSql('SELECT * FROM TagHistory', [], function(t, r){
-
-         	console.log(r.rows);//'test_ok'
-      		
-   			});
+		/*db.readTransaction(function(t){
+			t.executeSql('SELECT * FROM TagHistory', [], function(t, r){
+			console.log(r.rows);//'test_ok'
+				
+		});
 		}, function(e){}); */
-    }
+	}
 });
 
